b2a8d4177f83b1a4313f6fe7b32c1762e49d985e
hugozanini <hugozaninigomes@gmail.com>
2017-08-14 14:49:19 -0300

    Update minimization.py

--- sc_cnn/src/minimization.py
+++ sc_cnn/src/minimization.py
@@ -1,3 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%% This file is part of SceneUnderstanding@CIARP. 
+% 
+% SceneUnderstanding@CIARP is free software: you can redistribute it and/or modify 
+% it under the terms of the GNU General Public License as published by 
+% the Free Software Foundation, either version 3 of the License, or 
+% (at your option) any later version. 
+% 
+% SceneUnderstanding@CIARP is distributed in the hope that it will be useful, 
+% but WITHOUT ANY WARRANTY; without even the implied warranty of 
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+% GNU General Public License for more details. 
+% 
+% You should have received a copy of the GNU General Public License 
+% along with SceneUnderstanding@CIARP. If not, see <http://www.gnu.org/licenses/>. 
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 import numpy as np
 import sys, os, spams, csv, time
 def cod2sparseSOMP(D,X,L = 20, numThreads = -1):
